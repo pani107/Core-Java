33ce5b4ff15e6d433b6f624d84dbe0037c38c359
Pranav Wadkar <pranavwadkar2335@gmail.com>
2025-08-22 20:33:36 +0530

    Update if.java
    add comment.

--- if.java
+++ if.java
@@ -1,8 +1,7 @@
 public class IfExample {
     public static void main(String[] args) {
         int number = 10;
-
-        // if statement
+        // if statement.
         if (number > 0) {
             System.out.println("The number is positive.");
         }
