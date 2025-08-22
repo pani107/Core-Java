6003d04a2485b2210faa5e02fc04e75f797533fb
Pranav Wadkar <pranavwadkar2335@gmail.com>
2025-08-22 20:32:23 +0530

    Update NestedTryCatchExample.java
    add comment.

--- NestedTryCatchExample.java
+++ NestedTryCatchExample.java
@@ -1,17 +1,17 @@
 public class NestedTryCatchExample {
     public static void main(String[] args) {
-        // Outer try block
+        // Outer try block.
         try {
             int arr[] = {10, 20, 30};
 
-            // Inner try block 1
+            // Inner try block 1.
             try {
                 System.out.println("Accessing element at index 5: " + arr[5]);
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Inner Catch 1: Array index is out of bounds!");
             }
 
-            // Inner try block 2
+            // Inner try block 2.
             try {
                 int num = 10 / 0;
             } catch (ArithmeticException e) {
@@ -21,7 +21,7 @@ public class NestedTryCatchExample {
             System.out.println("End of inner try blocks.");
 
         } catch (Exception e) {
-            // Outer catch block
+            // Outer catch block.
             System.out.println("Outer Catch: Exception occurred - " + e);
         }
 
