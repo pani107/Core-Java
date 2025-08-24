c2026f44646add49387942a0f735ba1dbca8b5c9
Pranav Wadkar <pranavwadkar2335@gmail.com>
2025-08-24 18:18:17 +0530

    Create MyThread .java
    Creating Thread by extending Thread class.

---
+++ MyThread .java
@@ -0,0 +1,12 @@
+class MyThread extends Thread {
+    public void run() {
+        for(int i=1; i<=5; i++) {
+            System.out.println("Thread running: " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start(); // start() calls run() internally
+    }
+}
