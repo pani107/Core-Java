7416c312e34c295447e759a9d3e919dd92e54587
Pranav Wadkar <pranavwadkar2335@gmail.com>
2025-08-24 18:19:42 +0530

    Create Worker.java
    Running two threads together.

---
+++ Worker.java
@@ -0,0 +1,23 @@
+class Worker extends Thread {
+    public void run() {
+        for(int i=1; i<=5; i++) {
+            System.out.println(Thread.currentThread().getName() + " - " + i);
+            try {
+                Thread.sleep(500); // pause for half second
+            } catch(Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Worker t1 = new Worker();
+        Worker t2 = new Worker();
+        
+        t1.setName("Worker-1");
+        t2.setName("Worker-2");
+
+        t1.start();
+        t2.start();
+    }
+}
