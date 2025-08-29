067d02b0299579e6b22d956923f937fdd9544899
Pranav Wadkar <pranavwadkar2335@gmail.com>
2025-08-29 21:01:27 +0530

    Update ThrowExample.java
    add comment.

--- ThrowExample.java
+++ ThrowExample.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class ThrowExample {
 
-    // A method that checks age and throws exception if invalid
+    // A method that checks age and throws exception if invalid.
     static void checkAge(int age) {
         if (age < 18) {
-            // Throwing an exception object manually
+            // Throwing an exception object manually.
             throw new ArithmeticException("Access denied - You must be at least 18 years old.");
         } else {
             System.out.println("Access granted - You are old enough!");
