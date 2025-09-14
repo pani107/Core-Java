71e15db189e3bfcf7e5b4d7605564ce2a545b189
Pranav Wadkar <pranavwadkar2335@gmail.com>
2025-09-14 19:03:44 +0530

    Update MyRunnable.java
    add comment

--- MyRunnable.java
+++ MyRunnable.java
@@ -7,7 +7,7 @@ class MyRunnable implements Runnable {
 
     public static void main(String[] args) {
         MyRunnable obj = new MyRunnable();
-        Thread t1 = new Thread(obj); // pass Runnable object
+        Thread t1 = new Thread(obj); // pass Runnable object.
         t1.start();
     }
 }
