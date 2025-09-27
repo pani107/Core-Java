eb44d26cd7edcb7c9145d243a5bff9f2294afa87
Pranav Wadkar <pranavwadkar2335@gmail.com>
2025-09-27 20:45:40 +0530

    Update FinallyExample.java
    add comment

--- FinallyExample.java
+++ FinallyExample.java
@@ -1,7 +1,7 @@
 public class FinallyExample {
     public static void main(String[] args) {
         try {
-            int data = 10 / 2;  // No exception
+            int data = 10 / 2;  // No exception.
             System.out.println("Result: " + data);
         } catch (ArithmeticException e) {
             System.out.println("Exception caught: " + e);
