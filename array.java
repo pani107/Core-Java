cdfc2e85dbb6295639bc7b833dd769e2def0c203
Pranav Wadkar <pranavwadkar2335@gmail.com>
2025-10-07 22:59:26 +0530

    Update array.java
    Add comments

--- array.java
+++ array.java
@@ -4,7 +4,7 @@ public class ArrayExample {
         // 1️ Declaration and initialization
         int[] numbers = {10, 20, 30, 40, 50};
 
-        // 2️ Accessing elements by index
+        // 2️ Accessing elements by index.
         System.out.println("First element: " + numbers[0]);
         System.out.println("Last element: " + numbers[numbers.length - 1]);
 
